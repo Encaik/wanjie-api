2e6162e74c52bfa64c0acd195bc9871c90cd2826
shatong <843391622@qq.com>
2024-09-29 16:10:06 +0800

    feat(story): 添加剧情生成

--- app/api/generate/story/route.ts
+++ app/api/generate/story/route.ts
@@ -1,28 +1,27 @@
-import { ResStatus } from "../../common/model";
-import { NextRequest } from "next/server";
-import { getCharacterChat } from "@utils/kimi";
+import { ResStatus } from '../../common/model';
+import { NextRequest } from 'next/server';
+import { getCharacterChat } from '@utils/kimi';
 
-export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const name = searchParams.get("name");
-  if (!name) {
+export async function POST(request: NextRequest) {
+  const { character, env } = await request.json();
+  if (!character||!env) {
     return Response.json({
       status: ResStatus.ServerError,
-      msg: "参数错误",
-      data: null,
+      msg: '参数错误',
+      data: null
     });
   }
   try {
     return Response.json({
       status: ResStatus.Success,
-      msg: "ai背景故事生成",
-      data: await getCharacterChat(name),
+      msg: 'ai背景故事生成',
+      data: await getCharacterChat(character,env)
     });
   } catch (error) {
     return Response.json({
       status: ResStatus.ServerError,
       msg: error,
-      data: null,
+      data: null
     });
   }
 }
--- app/utils/kimi.ts
+++ app/utils/kimi.ts
@@ -1,38 +1,39 @@
-import { createClient } from "@vercel/kv";
-import OpenAI from "openai";
+import { createClient } from '@vercel/kv';
+import OpenAI from 'openai';
+import { Env, InitCharacter } from '../models';
 
 const kv = createClient({
   url: process.env.WANJIE_API_REST_API_URL,
   token: process.env.WANJIE_API_REST_API_TOKEN,
-  automaticDeserialization: false,
+  automaticDeserialization: false
 });
 
-export async function getCharacterChat(name: string) {
+export async function getCharacterChat(character: InitCharacter, env: Env) {
   return await getChat(
-    "你是读过所有网络小说，精通网络小说套路的专家，现在我要给你一个角色，你需要用你擅长的套路，生成一个角色的背景故事，请用中文回答，字数控制在90-120字。",
-    `主角的名字叫${name}`
+    '你是读过所有网络小说,精通网络小说套路的专家,避免在回答中存在不符合小说内容的语句，你的回答要让人有阅读小说的沉浸感,你需要用你擅长的套路,生成一个角色的背景故事,请用中文回答,字数控制在90-120字',
+    `主角的信息如下：${JSON.stringify(character)}\n环境信息如下：${JSON.stringify(env)},请使用这些信息生成出一个游戏的初始化背景故事`
   );
 }
 
 async function getChat(system: string, role: string) {
-  const token: string | null = await kv.get("KIMI_TOKEN");
+  const token: string | null = await kv.get('KIMI_TOKEN');
   if (!token) {
-    throw new Error("token 错误");
+    throw new Error('token 错误');
   }
   const client = new OpenAI({
     apiKey: token,
-    baseURL: "https://api.moonshot.cn/v1",
+    baseURL: 'https://api.moonshot.cn/v1'
   });
   const completion = await client.chat.completions.create({
-    model: "moonshot-v1-8k",
+    model: 'moonshot-v1-8k',
     messages: [
       {
-        role: "system",
-        content: system,
+        role: 'system',
+        content: system
       },
-      { role: "user", content: role },
+      { role: 'user', content: role }
     ],
-    temperature: 0.3,
+    temperature: 0.3
   });
   return completion.choices[0].message.content;
 }
